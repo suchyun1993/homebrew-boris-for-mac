6255a01cde6b09a46a70550cc48de97948327456
yun <yun@yuns-Mac-mini.local>
2026-03-24 21:50:36 +0800

    cask: switch URLs to BORIS_for_mac repo

--- Casks/boris-for-mac.rb
+++ Casks/boris-for-mac.rb
@@ -4,10 +4,10 @@ cask "boris-for-mac" do
   version "2.0.1"
   sha256 arm: "8cbb764fe0afc127e128549d815e9c2eb5c199946ef575a9e75de334def0dd3b"
 
-  url "https://github.com/suchyun1993/BORIS_for_Mac/releases/download/v#{version}/BORIS_for_Mac-#{version}-#{arch}.dmg"
+  url "https://github.com/suchyun1993/BORIS_for_mac/releases/download/v#{version}/BORIS_for_Mac-#{version}-#{arch}.dmg"
   name "BORIS_for_Mac"
   desc "BORIS embedded-video macOS build"
-  homepage "https://github.com/suchyun1993/BORIS_for_Mac"
+  homepage "https://github.com/suchyun1993/BORIS_for_mac"
 
   depends_on arch: :arm64
   depends_on formula: "ffmpeg"
