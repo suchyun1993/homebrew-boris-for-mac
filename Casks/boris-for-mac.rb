082f02c0371722c9c059c912cb54ea0efbbf2b69
yun <yun@yuns-Mac-mini.local>
2026-03-25 11:58:21 +0800

    cask: bump to 2.0.2

--- Casks/boris-for-mac.rb
+++ Casks/boris-for-mac.rb
@@ -1,8 +1,8 @@
 cask "boris-for-mac" do
   arch arm: "arm64"
 
-  version "2.0.1"
-  sha256 arm: "8cbb764fe0afc127e128549d815e9c2eb5c199946ef575a9e75de334def0dd3b"
+  version "2.0.2"
+  sha256 arm: "8f4042847000cd189bbe9fb434a8ff8127357b5f8574d6b283127aab303657f3"
 
   url "https://github.com/suchyun1993/BORIS_for_mac/releases/download/v#{version}/BORIS_for_Mac-#{version}-#{arch}.dmg"
   name "BORIS_for_Mac"
